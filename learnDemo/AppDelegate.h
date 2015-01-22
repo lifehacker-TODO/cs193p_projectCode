eb40ed309ad6a61d326b357c339ce4081bd33369
huliyou <liyoupie@gmail.com>
2015-01-22 13:03:32 +0800

    Initial Commit

---
+++ learnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  learnDemo
+//
+//  Created by Leiyou Who on 1/22/15.
+//  Copyright (c) 2015 Leiyou Who. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ learnDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  learnDemo
+//
+//  Created by Leiyou Who on 1/22/15.
+//  Copyright (c) 2015 Leiyou Who. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
